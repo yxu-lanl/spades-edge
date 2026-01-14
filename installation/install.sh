c7e2a21ae6d1d11820e696a68b0fe51394cfd8c0
Yan Xu <yxu@pn2405079.lanl.gov>
2026-01-14 11:35:29 -0700

    Revert "Update install.sh"
    This reverts commit 3edbc4f847b2697cc3b505ae1285dd26510fd527.

--- installation/install.sh
+++ installation/install.sh
@@ -95,5 +95,8 @@ npm install
 
 echo "LANL EDGE webapp successfully installed!"
 echo "Next steps:"
-echo "1. start MongoDB if it's not started yet"
-echo "2. start the webapp in EDGE's root directory: pm2 start pm2.config.js"
+echo "1. copy webapp/client/.env.example to webapp/client/.env and update settings in the .env file"
+echo "2. inside webapp/client, run command: npm run build"
+echo "3. copy webapp/server/.env.example to webapp/server/.env and update settings in the .env file"
+echo "4. start MongoDB if it's not started yet"
+echo "5. start the webapp in EDGE's root directory: pm2 start pm2.config.js"
